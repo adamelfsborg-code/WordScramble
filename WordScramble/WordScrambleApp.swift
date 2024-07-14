a52113ad799b96154cd3e9441989d99f8f90c506
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-14 09:55:54 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Adam Elfsborg on 2024-07-14.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
